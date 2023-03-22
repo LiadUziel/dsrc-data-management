7a953ea8ac5f311e188f982fd5d62aacff2a364d
Liad Uziel <liad.uz@gmail.com>
2023-03-22 18:49:23 +0200

    init primeng & SharedModule

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+
+
+// primeng
+import { ButtonModule } from 'primeng/button';
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        // primeng
+        ButtonModule
+    ],
+    exports: [
+        // primeng
+        ButtonModule
+    ]
+})
+
+export class SharedModule {}
