0c6037f0911275b3d7fe9f4ae384cadea40ef878
Liad Uziel <liad.uz@gmail.com>
2023-03-30 22:04:50 +0300

    create errorHandler middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { Config } from "./config/config";
+import { errorHandler } from "./controllers/error.controller";
 
 let str: string;
 str = "check ts-node-dev";
@@ -25,9 +26,17 @@ app.use(express.json());
 
 // 'Hello Express' endpoint - //* http://localhost:3000/api
 app.use("/api", async (req, res, next) => {
-  console.log("🚀 ~ file: index.ts:30 ~ app.use ~ req.body:", req.body);
-  return res.send("Hello Express");
+  try {
+    console.log("🚀 ~ file: index.ts:30 ~ app.use ~ req.body:", req.body);
+    // throw new Error("EROROROROROR");
+    return res.send("Hello Express");
+  } catch (err) {
+    next(err);
+  }
 });
+
+// Error handler
+app.use(errorHandler);
 
 async function main() {
   try {
