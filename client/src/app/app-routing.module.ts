6fc8eeb4d02b2e6ecbf469a18704a064557e9b85
Liad Uziel <liad.uz@gmail.com>
2023-04-18 09:08:58 +0300

    add submit proposal components to navbar and routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { DsDoctoralComponent } from './pages/submit-proposal/ds-doctoral/ds-doctoral.component';
+import { PostDoctoralComponent } from './pages/submit-proposal/post-doctoral/post-doctoral.component';
+import { SeedResearchComponent } from './pages/submit-proposal/seed-research/seed-research.component';
+import { DatasetCollectionComponent } from './pages/submit-proposal/dataset-collection/dataset-collection.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+
+  // auth
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+
+  // submit proposal
+  { path: 'submit-proposal/ds-doctoral', component: DsDoctoralComponent },
+  { path: 'submit-proposal/post-doctoral', component: PostDoctoralComponent },
+  { path: 'submit-proposal/seed-research', component: SeedResearchComponent },
+  {
+    path: 'submit-proposal/dataset-collection',
+    component: DatasetCollectionComponent,
+  },
+
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- client/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ client/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,41 +1,60 @@
-import { Component ,OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
   items: MenuItem[];
 
   ngOnInit() {
     this.items = [
-        {
-            label: 'DSRC Data Management',
-            disabled: true
-        },
-        {
-            label: 'Home',
-            icon: 'pi pi-fw pi-home',
-            routerLink: 'login'
-        },
-        {
-            label: 'Sign Up',
-            routerLink: 'register'
-        },
-        {
-            label: 'Sign In',
-            routerLink: 'login'
-        },
-        {
-            label: 'Log Out',
-            icon: 'pi pi-fw pi-sign-out',
-            routerLink: 'login'
-        }
+      {
+        label: 'DSRC Data Management',
+        disabled: true,
+      },
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: 'login',
+      },
+      {
+        label: 'Sign Up',
+        routerLink: 'register',
+      },
+      {
+        label: 'Sign In',
+        routerLink: 'login',
+      },
+      {
+        label: 'Log Out',
+        icon: 'pi pi-fw pi-sign-out',
+        routerLink: 'login',
+      },
+      {
+        label: 'Submit Proposal',
+        icon: 'pi pi-plus-circle',
+        items: [
+          {
+            label: 'Data Science Doctoral',
+            routerLink: 'submit-proposal/ds-doctoral',
+          },
+          {
+            label: 'Post-Doctoral research',
+            routerLink: 'submit-proposal/post-doctoral',
+          },
+          {
+            label: 'Seed research',
+            routerLink: 'submit-proposal/seed-research',
+          },
+          {
+            label: 'Dataset collection',
+            routerLink: 'submit-proposal/dataset-collection',
+          },
+        ],
+      },
     ];
+  }
 }
-}
-
-
