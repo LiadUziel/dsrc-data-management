fb7a14ce58573c4320da29b90f4746390c5f640c
Liad Uziel <liad.uz@gmail.com>
2023-04-22 11:44:03 +0300

    get grant proposal endpoint in API

--- server/src/controllers/grant-proposal.controller.ts
+++ server/src/controllers/grant-proposal.controller.ts
@@ -15,3 +15,28 @@ export const createGrantProposal: RequestHandler = async (req, res, next) => {
     next(e);
   }
 };
+
+// get proposals by type or all of them from db
+export const getGrantProposals: RequestHandler = async (req, res, next) => {
+  try {
+    let { type } = req.params;
+
+    // check if type is legal
+    type = type?.toUpperCase();
+    const isLegalType: boolean = [
+      "DS_DOCTORAL",
+      "POST_DOCTORAL",
+      "SEED_RESEARCH",
+      "DATASET_COLLECTION",
+    ].includes(type);
+
+    // get proposals from db
+    const proposals = await GrantProposalModel.find(
+      isLegalType ? { type } : {}
+    );
+
+    return res.send(proposals);
+  } catch (e) {
+    next(e);
+  }
+};
