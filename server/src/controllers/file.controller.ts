956b6b17ca012f52598f82ff093172827dc4c114
Liad Uziel <liad.uz@gmail.com>
2024-02-17 18:51:06 +0200

    upload file to firebase storage instead local

--- server/src/controllers/file.controller.ts
+++ server/src/controllers/file.controller.ts
@@ -1,19 +1,34 @@
+import { Request, Response } from "express";
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import { bucket } from "../config/storage-config";
 
-const destination: string = "../files";
-let currentDateString;
-let fileName;
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, destination); // Specify the destination folder where the file will be saved
-  },
-  filename: function (req, file, cb) {
-    const nowDate = new Date();
-    currentDateString = `${new Date().toLocaleDateString()} ${nowDate.getHours()}_${nowDate.getMinutes()}_${nowDate.getSeconds()}`;
-    fileName = `${currentDateString} ${file.originalname}`;
-    cb(null, fileName); // Use the original file name as part of the saved file name
-  },
-});
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Middleware configuration settings for upload
+export const uploadFile = upload.single("file");
+
+export const uploadFileToFirebase = async (req: Request, res: Response) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+
+    const fileName = `${uuidv4()}_${req.file.originalname}`;
+    const blob = bucket.file(fileName);
+
+    await blob.save(req.file.buffer, {
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    });
+
+    return res.status(200).send({ fileName });
+  } catch (err) {
+    return res.status(500).send("Something went wrong with the file upload.");
+  }
+};
+
 export let upload = multer({ storage });
 
 
--- server/src/routes/file.router.ts
+++ server/src/routes/file.router.ts
@@ -4,33 +4,6 @@ const path = require('path');
 
 const fileRouter = Router();
 
-fileRouter.post(
-  "/upload",
-  upload.single("file"),
-  async (req: any, res, next) => {
-    try {
-      const uploadedFile = req.file;
-      const response = {
-        message: 'File uploaded successfully.',
-        filepath: `${uploadedFile.path}`,
-      };
-  
-      return res.json(response);
-    } catch (err) {
-      next(err);
-    }
-  }
-);
-
-fileRouter.post('/download', async(req, res, next) => {
-  try {
-    
-    const fileAbsolutePath = path.join(path.dirname(path.dirname(__dirname)), req.body.filePath);
-    res.sendFile(fileAbsolutePath);
-  }
-  catch (err) {
-    next(err);
-  }
-});
+fileRouter.post("/upload", uploadFile, uploadFileToFirebase);
 
 export default fileRouter;
